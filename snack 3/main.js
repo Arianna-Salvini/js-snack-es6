87a16a35d2b10f83e121f196ae9ddfee5d63ff09
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-05 16:30:30 +0100

    Add Snack 3

--- snack 3/main.js
+++ snack 3/main.js
@@ -1,1 +1,34 @@
 console.log('Sneak 3');
+
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+
+const animals = [
+    {
+        nome: 'leone',
+        famiglia: 'felidi',
+        classe: 'mammiferi'
+    },
+
+    {
+        nome: 'cane',
+        famiglia: 'canidi',
+        classe: 'mammiferi'
+    },
+
+    {
+        nome: 'gallina',
+        famiglia: 'fasianidi',
+        classe: 'uccelli'
+    },
+]
+
+// Crea un nuovo array con la lista dei mammiferi.
+
+// const mammal = animals.filter(animal => {
+//     if(animal.classe === 'mammiferi'){
+//         return ;
+//     }
+// })
+const mammal = animals.filter(animal => animal.classe=== 'mammiferi')
+console.log(mammal);
